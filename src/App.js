4996e927390646f08d636f7ff9fdd2120549ee26
balaji1610 <balajiece1610@gmail.com>
2023-05-31 22:51:38 +0530

    Add_bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div> Todo APP</div>;
+  return (
+    <div>
+      {" "}
+      Todo APP
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
 }
 
 export default App;
