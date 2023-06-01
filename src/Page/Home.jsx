d9db2d7a4908e172de2bfe89b17737df3901657d
balaji1610 <balajiece1610@gmail.com>
2023-06-01 11:52:47 +0530

    Get_structure_Add_screenModule

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,16 +1,20 @@
 import TopHomeScreen from "../Containers/TopHomeScreen";
 import AddScreen from "../Containers/AddScreen";
 import { useState } from "react";
-// import { Todoitems } from "../Utillis/Todoitems";
+import { Todoitems } from "../Utillis/Todoitems";
 let id = 1;
 export default function Home() {
-  const [Dataarray, setDataArray] = useState([
-    { id: "", taskname: "", status: "" },
-  ]);
+  const [Dataarray, setDataArray] = useState(Todoitems);
 
   //AddscrenModule
   const [addmodel, setModel] = useState(false);
-  const AddscrenModule = (function () {
+
+  //AddModel
+  const [textarea, setTextArea] = useState("");
+  const [getStatus, setGetStatus] = useState("");
+
+  //Add screenMoudle Operations
+  const AddscrenModuleOperations = (function () {
     const handleAddicon = () => {
       setModel(true);
       console.log(Dataarray);
@@ -20,39 +24,45 @@ export default function Home() {
       setModel(false);
     };
 
+    const changeTextarea = (e) => {
+      const value = e.target.value;
+      if (value.length <= 25) {
+        setTextArea(value);
+      }
+    };
+
+    const changeGetValue = (e) => {
+      setGetStatus(e.target.value);
+    };
+
+    const SubmitHandleClick = (e) => {
+      e.preventDefault();
+      setTextArea("");
+      setGetStatus("");
+      const update = [
+        ...Dataarray,
+        { id: id++, taskname: textarea, status: getStatus },
+      ];
+
+      setDataArray(update);
+    };
+
     return {
       handleAddicon,
       handleCancelAddicon,
+      changeTextarea,
+      changeGetValue,
+      SubmitHandleClick,
     };
   })();
-  const { handleAddicon, handleCancelAddicon } = AddscrenModule;
+  const {
+    handleAddicon,
+    handleCancelAddicon,
+    changeTextarea,
+    changeGetValue,
+    SubmitHandleClick,
+  } = AddscrenModuleOperations;
 
-  //AddModel
-  const [textarea, setTextArea] = useState("");
-  const [getStatus, setGetStatus] = useState("");
-
-  const changeTextarea = (e) => {
-    const value = e.target.value;
-    if (value.length <= 25) {
-      setTextArea(value);
-    }
-  };
-
-  const changeGetValue = (e) => {
-    setGetStatus(e.target.value);
-  };
-
-  const SubmitHandleClick = (e) => {
-    e.preventDefault();
-    setTextArea("");
-    setGetStatus("");
-    const update = [
-      ...Dataarray,
-      { id: id++, taskname: textarea, status: getStatus },
-    ];
-
-    setDataArray(update);
-  };
   console.log(Dataarray, "Dataarray");
   return (
     <div>
